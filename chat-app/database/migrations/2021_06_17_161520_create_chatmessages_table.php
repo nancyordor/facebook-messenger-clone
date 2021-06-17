d2b9e3a42c2f0e8b7429fc97e7269fa5e025e41d
nancyordor <chinyereordor@gmail.com>
2021-06-17 17:28:30 +0100

    Initial Commit

---
+++ chat-app/database/migrations/2021_06_17_161520_create_chatmessages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatmessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chatmessages', function (Blueprint $table) {
+            $table->id();
+            $table->integer('chat_room_id');
+            $table->integer('user_id');
+            $table->mediumText('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chatmessages');
+    }
+}
